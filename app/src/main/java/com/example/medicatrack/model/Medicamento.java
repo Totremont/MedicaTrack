cc55b0f1522489d9b40dcc9db65c3c16b6b01103
Ezequiel <ezequielmizaguirre@gmail.com>
2023-01-30 12:33:46 -0300

    Setup inicial

---
+++ app/src/main/java/com/example/medicatrack/model/Medicamento.java
@@ -0,0 +1,12 @@
+package com.example.medicatrack.model;
+
+public class Medicamento
+{
+    private int id;
+    private String nombre;
+    private String color;
+    private String forma;
+    private float concentracion;
+    private String frecuencia;
+
+}
---
+++ app/src/main/java/com/example/medicatrack/model/Registro.java
@@ -0,0 +1,11 @@
+package com.example.medicatrack.model;
+
+import java.time.LocalDateTime;
+
+public class Registro
+{
+    private int id;
+    private Medicamento medicamento;
+    private LocalDateTime fecha;
+    private boolean confirmado;
+}
