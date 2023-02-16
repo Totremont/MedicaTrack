014354ea6164a24f50e3db73336c48346158f1d2
Ezequiel <ezequielmizaguirre@gmail.com>
2023-02-16 19:46:10 -0300

    Agregadas preferencias locales de IDEA a gitignore

--- app/src/main/java/com/example/medicatrack/MainActivity.java
+++ app/src/main/java/com/example/medicatrack/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
